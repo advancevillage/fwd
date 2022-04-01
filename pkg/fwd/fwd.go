0bc2706f7619144375c66ce30384a3703d80a83e
richard.sun <richard.sun@ucloud.cn>
2022-04-01 11:20:21 +0800

    #richard.sun: fix

--- pkg/fwd/fwd.go
+++ pkg/fwd/fwd.go
@@ -3,6 +3,7 @@ package fwd
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net"
 
 	"github.com/advancevillage/3rd/logx"
@@ -23,7 +24,17 @@ type fwdCli struct {
 	valueSize int
 }
 
+type FwdElem struct {
+	Ip     string
+	Iface  uint32
+	SrcMac string
+	DstMac string
+}
+
 type IFwd interface {
+	QryFwd(ctx context.Context) ([]*FwdElem, error)
+	DelFwd(ctx context.Context, dstIp string) error
+	UptFwd(ctx context.Context, dstIp string, ifaceIndex uint32, srcmac string, dstmac string) error
 }
 
 func NewFwdClient(logger logx.ILogger) (IFwd, error) {
@@ -37,6 +48,47 @@ func NewFwdClient(logger logx.ILogger) (IFwd, error) {
 		valueSize: valueSize,
 		logger:    logger,
 	}, nil
+}
+
+//设置转发表
+//ifaceIndx   网络设备标示，表示从哪张设备转发
+//srcmac	  源MAC
+//dstmac	  目的MAC
+func (d *fwdCli) UptFwd(ctx context.Context, dstIp string, ifaceIndex uint32, srcmac string, dstmac string) error {
+	//1. 参数检查
+	ip, err := d.checkip(dstIp)
+	if err != nil {
+		return err
+	}
+	src, err := d.checkmac(srcmac)
+	if err != nil {
+		return err
+	}
+	dst, err := d.checkmac(dstmac)
+	if err != nil {
+		return err
+	}
+
+	k, v := d.kv(ip, ifaceIndex, src, dst)
+
+	err = d.update(ctx, k, v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *fwdCli) DelFwd(ctx context.Context, dstIp string) error {
+	ip, err := d.checkip(dstIp)
+	if err != nil {
+		return err
+	}
+	return d.delete(ctx, ip)
+}
+
+func (d *fwdCli) QryFwd(ctx context.Context) ([]*FwdElem, error) {
+	return d.query(ctx)
 }
 
 func (i *fwdCli) update(ctx context.Context, key []byte, value []byte) error {
@@ -58,6 +110,90 @@ func (i *fwdCli) update(ctx context.Context, key []byte, value []byte) error {
 		return err
 	}
 	return nil
+}
+
+func (i *fwdCli) delete(ctx context.Context, key []byte) error {
+	var err error
+	if !i.tableCli.ExistTable(ctx) {
+		err = i.tableCli.CreateTable(ctx)
+	}
+	if err != nil {
+		return err
+	}
+	if len(key) != i.keySize {
+		return errors.New("key size is invalid")
+	}
+	err = i.tableCli.DeleteTable(ctx, key)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (i *fwdCli) query(ctx context.Context) ([]*FwdElem, error) {
+	var r = make([]*FwdElem, 0, 2)
+	if i.tableCli == nil {
+		return r, nil
+	}
+	if !i.tableCli.ExistTable(ctx) {
+		return r, nil
+	}
+	var kv, err = i.tableCli.QueryTable(ctx)
+	if err != nil {
+		return r, err
+	}
+	for i := range kv {
+		var (
+			kk = kv[i].Key
+			vv = kv[i].Value
+			rr = new(FwdElem)
+		)
+		rr.Ip = net.IPv4(kk[3], kk[2], kk[1], kk[0]).String()
+		rr.Iface |= uint32(vv[0])
+		rr.Iface |= uint32(vv[1]) << 8
+		rr.Iface |= uint32(vv[2]) << 16
+		rr.Iface |= uint32(vv[3]) << 24
+		rr.SrcMac = fmt.Sprintf("%x:%x:%x:%x:%x:%x", vv[4], vv[5], vv[6], vv[7], vv[8], vv[9])
+		rr.DstMac = fmt.Sprintf("%x:%x:%x:%x:%x:%x", vv[0xa], vv[0xb], vv[0xc], vv[0xd], vv[0xe], vv[0xf])
+
+		r = append(r, rr)
+	}
+	return r, nil
+}
+
+func (d *fwdCli) kv(ip []byte, ifaceIndex uint32, src []byte, dst []byte) ([]byte, []byte) {
+	var k = make([]byte, d.keySize)
+	var v = make([]byte, d.valueSize)
+
+	copy(k, ip)
+
+	v[0] = byte(ifaceIndex)
+	v[1] = byte(ifaceIndex >> 8)
+	v[2] = byte(ifaceIndex >> 16)
+	v[3] = byte(ifaceIndex >> 24)
+	copy(v[4:10], src)
+	copy(v[10:16], dst)
+
+	return k, v
+}
+
+func (d *fwdCli) checkip(ip string) ([]byte, error) {
+	netip := net.ParseIP(ip)
+	if netip == nil {
+		return nil, errors.New("invalid ip format")
+	}
+	addr := netip.To4()
+	if addr == nil {
+		return nil, errors.New("invalid ip format")
+	}
+	b := make([]byte, 4)
+
+	b[0] = addr[3]
+	b[1] = addr[2]
+	b[2] = addr[1]
+	b[3] = addr[0]
+
+	return b, nil
 }
 
 //eg: 08:00:27:f3:81:0e
--- pkg/fwd/fwd_test.go
+++ pkg/fwd/fwd_test.go
@@ -1,9 +1,11 @@
 package fwd
 
 import (
+	"context"
 	"net"
 	"testing"
 
+	"github.com/advancevillage/3rd/logx"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -43,4 +45,112 @@ func Test_mac_check(t *testing.T) {
 		}
 		t.Run(n, f)
 	}
+}
+
+var ipTest = map[string]struct {
+	dstIp string
+	exp   []byte
+	err   error
+}{
+	"case1": {
+		dstIp: "127.0.0.1",
+		exp:   []byte{0x01, 0x00, 0x00, 0x7f},
+	},
+}
+
+func Test_ip_check(t *testing.T) {
+	var c = &fwdCli{}
+
+	for n, p := range ipTest {
+		f := func(t *testing.T) {
+			var act, err = c.checkip(p.dstIp)
+			if err != nil {
+				assert.Equal(t, p.err, err)
+			} else {
+				assert.Equal(t, p.exp, act)
+			}
+		}
+		t.Run(n, f)
+	}
+}
+
+var kvTest = map[string]struct {
+	ip    []byte
+	src   []byte
+	dst   []byte
+	iface uint32
+	k     []byte
+	v     []byte
+}{
+	"case1": {
+		ip:    []byte{0x01, 0x00, 0x00, 0x7f},
+		src:   []byte{0x08, 0x00, 0x27, 0xf3, 0x81, 0x0e},
+		dst:   []byte{0xf8, 0xf0, 0x27, 0xf3, 0x81, 0x0e},
+		iface: 4,
+		k:     []byte{0x01, 0x00, 0x00, 0x7f},
+		v:     []byte{0x04, 0x00, 0x00, 0x00, 0x08, 0x00, 0x27, 0xf3, 0x81, 0x0e, 0xf8, 0xf0, 0x27, 0xf3, 0x81, 0x0e},
+	},
+}
+
+func Test_kv_check(t *testing.T) {
+	var c = &fwdCli{
+		keySize:   keySize,
+		valueSize: valueSize,
+	}
+
+	for n, p := range kvTest {
+		f := func(t *testing.T) {
+			var k, v = c.kv(p.ip, p.iface, p.src, p.dst)
+			assert.Equal(t, p.k, k)
+			assert.Equal(t, p.v, v)
+		}
+		t.Run(n, f)
+	}
+}
+
+var uptTest = map[string]struct {
+	dstIp string
+	src   string
+	dst   string
+	iface uint32
+	exp   []*FwdElem
+}{
+	"case1": {
+		dstIp: "192.168.1.103",
+		src:   "08:00:27:f3:81:0e",
+		dst:   "f8:ff:27:f3:81:0e",
+		iface: 4,
+		exp:   []*FwdElem{},
+	},
+}
+
+func Test_fwd_update(t *testing.T) {
+	logger, err := logx.NewLogger("info")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	c, err := NewFwdClient(logger)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	for n, p := range uptTest {
+		f := func(t *testing.T) {
+			var err = c.UptFwd(context.TODO(), p.dstIp, p.iface, p.src, p.dst)
+			if err != nil {
+				t.Fatal(err)
+				return
+			}
+			r, err := c.QryFwd(context.TODO())
+			if err != nil {
+				t.Fatal(err)
+				return
+			}
+			assert.Equal(t, p.exp, r)
+		}
+		t.Run(n, f)
+	}
+
 }
